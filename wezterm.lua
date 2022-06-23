369d1f2e7620e1c92faf3f435701a2bdf091d78f
mudong <feng2r200@qq.com>
2022-06-23 11:04:29 +0800

    fix: ignore bell

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 
 return {
     adjust_window_size_when_changing_font_size = false,
+    audible_bell="Disabled",
     color_scheme = "Dracula+",
     disable_default_key_bindings = true,
     enable_tab_bar = false,
